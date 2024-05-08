b161d6fbc705ca2c86dae6519b1bec3f5d0d0daa
DevvUdit01 <devv.udit@gmail.com>
2024-05-08 12:32:53 +0530

    first branch complete

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -40,7 +41,7 @@ class HomePage extends StatelessWidget {
                                   children: [
                                     Text(
                                       "Udit Pal",
-                                      style: TextStyle(
+                                      style: GoogleFonts.cousine(
                                         fontSize: 25,
                                         fontWeight: FontWeight.bold,
                                         color: Colors.white,
